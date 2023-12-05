b8ae6d89893aa5100cdf28c41dd1f17e6ae9feed
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2023-12-05 17:44:00 -0600

    npx prettier -w next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/?page_id=33', // Locations
-                destination: '/locations',
-                permanent: true,
-            },
-            {
-                source: '/?page_id=18', // About
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: '/?page_id=48', // FAQ
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: '/?page_id=19', // CONTACT
-                destination: '/',
-                permanent: false,
-            },
-        ]
-    }
+  async redirects() {
+    return [
+      {
+        source: "/?page_id=33", // Locations
+        destination: "/locations",
+        permanent: true,
+      },
+      {
+        source: "/?page_id=18", // About
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/?page_id=48", // FAQ
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/?page_id=19", // CONTACT
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
