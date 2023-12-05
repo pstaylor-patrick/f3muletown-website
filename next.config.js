d17df096bfea092ac70e8cf3622636175bb72835
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2023-12-05 17:47:46 -0600

    Revert "add redirects for old BlueHost WordPress paths"
    This reverts commit 645589316584d6eb8ab9f4d68cffc4388a05104b.

--- next.config.js
+++ next.config.js
@@ -1,29 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/?page_id=33', // Locations
-                destination: '/locations',
-                permanent: true,
-            },
-            {
-                source: '/?page_id=18', // About
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: '/?page_id=48', // FAQ
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: '/?page_id=19', // CONTACT
-                destination: '/',
-                permanent: false,
-            },
-        ]
-    }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
