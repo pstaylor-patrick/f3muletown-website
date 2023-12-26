c9048d54bbc21ca5b3ee1c8c709348ca9ad45e74
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2023-12-26 15:27:27 -0600

    tidy workouts page tsx for readability

--- src/app/workouts/page.tsx
+++ src/app/workouts/page.tsx
@@ -168,6 +168,8 @@ export default function fng() {
       time: WorkoutTimes["0600"],
     },
   ];
+  const today = new Date().getDay();
+  const saturday = 6;
   return (
     <>
       <Header href={href} />
@@ -194,6 +196,72 @@ export default function fng() {
           <Button href={mapUrl} text="VIEW FULL SCREEN" target="_blank" />
         </section>
         <section className={`bg-gloom leading-tight py-16 px-4`}>
+          {today === saturday ? (
+            <>
+              <h2 className="py-5">JOIN US</h2>
+              <ul>
+                {workoutsAnotherDay(workouts).map((w, i) => (
+                  <li key={i} className={i > 0 ? "pt-5" : ""}>
+                    <WorkoutCard
+                      ao={w.ao}
+                      q={w.q}
+                      avgAttendance={w.avgAttendance}
+                      style={w.style}
+                      location={w.location}
+                      day={w.day}
+                      time={w.time}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : (
+            <>
+              <h2>JOIN US TOMORROW</h2>
+              <p className="text-cmu pb-10">
+                Just show up and introduce yourself as an{" "}
+                <Link href="/fng">FNG</Link>, you will be welcomed!
+              </p>
+              <Button
+                href="https://f3near.me"
+                text="FIND NEAREST TO ME"
+                target="_blank"
+              />
+              <div className="my-10"></div>
+              <ul>
+                {workoutsTomorrow(workouts).map((w, i) => (
+                  <li key={i} className={i > 0 ? "pt-5" : ""}>
+                    <WorkoutCard
+                      ao={w.ao}
+                      q={w.q}
+                      avgAttendance={w.avgAttendance}
+                      style={w.style}
+                      location={w.location}
+                      day={w.day}
+                      time={w.time}
+                    />
+                  </li>
+                ))}
+              </ul>
+              <h2 className="py-5">OR ANOTHER TIME</h2>
+              <ul>
+                {workoutsAnotherDay(workouts).map((w, i) => (
+                  <li key={i} className={i > 0 ? "pt-5" : ""}>
+                    <WorkoutCard
+                      ao={w.ao}
+                      q={w.q}
+                      avgAttendance={w.avgAttendance}
+                      style={w.style}
+                      location={w.location}
+                      day={w.day}
+                      time={w.time}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+          {/* #region old */}
           {workoutsTomorrow(workouts).length > 0 ? (
             <>
               <h2>JOIN US TOMORROW</h2>
@@ -242,6 +310,7 @@ export default function fng() {
               </li>
             ))}
           </ul>
+          {/* #endregion old */}
         </section>
       </main>
       <Footer />
