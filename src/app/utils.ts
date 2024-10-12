242a68d85b8076ef9ba067f2a38669315182b259
Dave Staab <dave.staab@cadre5.com>
2024-10-12 14:55:40 -0400

    remove console logs

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -5,23 +5,18 @@ import { addWeeks, addYears, closestTo, compareAsc, parse } from "date-fns";
 export const dateFormat = 'EEEE HH:mm';
 
 export function sortWorkoutsNew(workouts: Workout[]): Workout[] {
-  console.log('now', getNow(), 'times', workouts.map(getNextTime));
   const sorted = workouts.sort((a, b) => {
     const dateCompare = compareAsc(getNextTime(a), getNextTime(b))
     return dateCompare !== 0 ? dateCompare : a > b ? 1 : -1;
   });
-  console.log('sorted', sorted.map(w => w.dayLabel));
   return [...sorted];
 }
 
 function getNextTime(workout: Workout) {
   const now = getNow();
   const dates = workout.times.flatMap((time) => {
-    console.log('time', time);
 
     return time.day.map((day) => {
-      console.log('day', day);
-
       const workoutDate = parse(`${day} ${time.start}`, dateFormat, now);
       // if the day has already passed for this week, it's for next week
       return compareAsc(now, workoutDate) === 1
@@ -29,8 +24,6 @@ function getNextTime(workout: Workout) {
         : workoutDate;
     });
   });
-  console.log('dates', dates);
-
   return closestTo(now, dates) ?? addYears(now, 100);
 }
 
