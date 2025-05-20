6738b1e447370724ba640921c34488c43f208c8a
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-05-20 15:46:41 -0500

    add google analytics code to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,28 @@
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
-import Head from 'next/head';
+import Script from 'next/script';
 
 import './globals.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="robots" content="noindex,nofollow" />
-      </Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-H3KTP1DXZF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H3KTP1DXZF');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.className} bg-iron text-white text-center font-sans text-lg`}
       >
